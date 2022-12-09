fd14d277c2b4ad3874d687096d3fd855bb5cad1b
Aletia88 <abababmgmgmg@gmail.com>
2022-12-09 23:16:36 +0300

    new file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # creating a new file using puppet
-file {'/tmp/school':
+
+file { '/tmp/school':
 content => 'I love Puppet',
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
 }
